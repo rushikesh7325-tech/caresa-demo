28e4ae761242bb1594d1002ebac5a65411eb7e7b
kiranlondhe432005 <kiranlondhe0143@gmail.com>
2026-02-07 16:14:30 +0530

    Add files via upload

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -2,16 +2,13 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static ThemeData lightTheme = ThemeData(
-    fontFamily: 'Poppins',
     scaffoldBackgroundColor: Colors.white,
     appBarTheme: const AppBarTheme(
       backgroundColor: Colors.white,
+      foregroundColor: Colors.black,
       elevation: 0,
-      iconTheme: IconThemeData(color: Colors.black),
     ),
-    textTheme: const TextTheme(
-      titleLarge: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
-      bodyMedium: TextStyle(fontSize: 14),
-    ),
+    primaryColor: Colors.black,
+    useMaterial3: true,
   );
 }
